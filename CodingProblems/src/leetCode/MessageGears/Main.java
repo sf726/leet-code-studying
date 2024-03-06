50c9d1a85234056303277f372de6791d38334d18
turtle-dov3 <73407100+turtle-dov3@users.noreply.github.com>
2024-03-05 20:06:35 -0500

    add comments to better understand reasoning

--- CodingProblems/src/leetCode/MessageGears/Main.java
+++ CodingProblems/src/leetCode/MessageGears/Main.java
@@ -11,13 +11,14 @@ package leetCode.MessageGears;
  */
 public class Main {
     public static int getTimeBasedOnDegree(int degrees){
-
+        // 360 / 12 is 30
         int hour = degrees / 30;
+        // every hour has 30 degrees, so 1 degree for every 2 minutes
         int minutes = degrees %10 *2;
         return hour * 100 + minutes;
     }
 
     public static void main(String[] args) {
-        System.out.println( getTimeBasedOnDegree(180));
+        System.out.println(getTimeBasedOnDegree(180));
     }
 }
